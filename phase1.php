39685eb73f01ad711117c2cb40c04a2b9cff398f
NoahK <nftrash@web.de>
2019-04-16 23:29:54 +0200

    PHP tests

--- phase1.php
+++ phase1.php
@@ -1,73 +1,158 @@
-<!DOCTYPE html>
-<html lang="de">
+<?php	// UTF-8 marker äöüÄÖÜß€
+/**
+ * Class PageTemplate for the exercises of the EWA lecture
+ * Demonstrates use of PHP including class and OO.
+ * Implements Zend coding standards.
+ * Generate documentation with Doxygen or phpdoc
+ *
+ * PHP Version 5
+ *
+ * @category File
+ * @package  Pizzaservice
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ * @license  http://www.h-da.de  none
+ * @Release  1.2
+ * @link     http://www.fbi.h-da.de
+ */
 
-  <head>
-    <meta charset="UTF-8">
-    <title>GenoChoice&trade; - GenoCheck&trade; bestellen</title>
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-    <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-  </head>
+// to do: change name 'PageTemplate' throughout this file
+require_once './Page.php';
 
-  <body>
-    <div>
+/**
+ * This is a template for top level classes, which represent
+ * a complete web page and which are called directly by the user.
+ * Usually there will only be a single instance of such a class.
+ * The name of the template is supposed
+ * to be replaced by the name of the specific HTML page e.g. baker.
+ * The order of methods might correspond to the order of thinking
+ * during implementation.
+
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ */
+class PageTemplate extends Page
+{
+    // to do: declare reference variables for members
+    // representing substructures/blocks
+
+    /**
+     * Instantiates members (to be defined above).
+     * Calls the constructor of the parent i.e. page class.
+     * So the database connection is established.
+     *
+     * @return none
+     */
+    protected function __construct()
+    {
+        parent::__construct();
+        // to do: instantiate members representing substructures/blocks
+    }
+
+    /**
+     * Cleans up what ever is needed.
+     * Calls the destructor of the parent i.e. page class.
+     * So the database connection is closed.
+     *
+     * @return none
+     */
+    protected function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    /**
+     * Fetch all data that is necessary for later output.
+     * Data is stored in an easily accessible way e.g. as associative array.
+     *
+     * @return none
+     */
+    protected function getViewData()
+    {
+        // to do: fetch data for this view from the database
+    }
+
+    protected function generateNavigationBar {
+        echo <<<EOT
+        <div>
       <ul class="navlist">
         <li class="active"><a href="#">Phase 1</a></li>
         <li><a href="#">Phase 2</a></li>
         <li><a href="#">Phase 3</a></li>
       </ul>
     </div>
+EOT;
+    }
 
-    <header>
-      <span class="headerTitle">Genochoice</span><br>
-      <span class="headerSubtitle">Legacy by Design</span>
-    </header>
+    /**
+     * First the necessary data is fetched and then the HTML is
+     * assembled for output. i.e. the header is generated, the content
+     * of the page ("view") is inserted and -if avaialable- the content of
+     * all views contained is generated.
+     * Finally the footer is added.
+     *
+     * @return none
+     */
+    protected function generateView()
+    {
+        $this->getViewData();
+        $this->generatePageHeader('GenoChoice&trade; - GenoCheck&trade; bestellen');
+        // to do: call generateView() for all members
+        // to do: output view of this page
 
-    <section>
-      <?php
-        echo "<span class=\"sectionHeader\">GenoCheck&trade; bestellen</span>";
-      ?>
-      <p>
-        Fordern Sie heute ihren <strong>kostenlosen</strong> GenoChoice&trade;-Gentest an.<br> Ein Team
-        aus professionellen Genforschern prüft mit unserem patentierten GenoCheck&trade;-Verfahren die
-        Stärken und Schwächen ihres zukünftigen Kindes.
-      </p>
-      <figure>
-        <img src="img/family.jpg" alt="Diese fröhliche Familie könnten Sie sein!">
-        <figcaption>Malte & Sombra Trontheim sind zufrieden mit ihrer GenoChoice&trade;-Entscheidung</figcaption>
-      </figure>
-    </section>
-    <section>
-      <span class="sectionHeader">
-        <?php
-        echo "Persönliche Daten";
-         ?>
-      </span>
-      <form id="genoCheckForm" name="genoCheckForm[]" action="phase1tophase2.html" method="post">
-        <label>Vorname</label>
-        <input type="text" id="inputFirstName" name="inputFirstName" value="Max" required autofocus>
+        $this->generateNavigationBar();
 
-        <label>Name</label>
-        <input type="text" id="inputLastName" name="inputLastName" value="Musterhalfen" required>
+        $this->generatePageFooter();
+    }
 
-        <label>Straße & Hausnummer</label>
-        <input type="text" id="inputStreet" name="inputStreet" value="Musterstraße" required>
+    /**
+     * Processes the data that comes via GET or POST i.e. CGI.
+     * If this page is supposed to do something with submitted
+     * data do it here.
+     * If the page contains blocks, delegate processing of the
+	 * respective subsets of data to them.
+     *
+     * @return none
+     */
+    protected function processReceivedData()
+    {
+        parent::processReceivedData();
+        // to do: call processReceivedData() for all members
+    }
 
-        <label>Stadt</label>
-        <input type="text" id="inputCity" name="inputCity" value="Musterstadt" required>
+    /**
+     * This main-function has the only purpose to create an instance
+     * of the class and to get all the things going.
+     * I.e. the operations of the class are called to produce
+     * the output of the HTML-file.
+     * The name "main" is no keyword for php. It is just used to
+     * indicate that function as the central starting point.
+     * To make it simpler this is a static function. That is you can simply
+     * call it without first creating an instance of the class.
+     *
+     * @return none
+     */
+    public static function main()
+    {
+        try {
+            $page = new PageTemplate();
+            $page->processReceivedData();
+            $page->generateView();
+        }
+        catch (Exception $e) {
+            header("Content-type: text/plain; charset=UTF-8");
+            echo $e->getMessage();
+        }
+    }
+}
 
-        <label>PLZ</label>
-        <input type="text" id="inputZipcode" name="inputZipcode" value="12345" pattern="\d{5}" required>
+// This call is starting the creation of the page.
+// That is input is processed and output is created.
+PageTemplate::main();
 
-        <label>E-Mail</label>
-        <input type="email" id="inputEmail" name="inputEmail" value="m.mustermann@gmail.com" required>
-
-        <button id="genoCheckSubmit" type="submit">
-          GenoCheck&trade; bestellen
-        </button>
-      </form>
-    </section>
-  </body>
-
-</html>
+// Zend standard does not like closing php-tag!
+// PHP doesn't require the closing tag (it is assumed when the file ends).
+// Not specifying the closing ? >  helps to prevent accidents
+// like additional whitespace which will cause session
+// initialization to fail ("headers already sent").
+//? >
