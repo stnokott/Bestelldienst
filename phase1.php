f7fdc4f0fd4e3dabde0283d1ec2a68d7aa60c771
NoahK <nftrash@web.de>
2019-04-16 15:13:10 +0200

    phase1 angepasst

--- phase1.php
+++ phase1.php
@@ -1,39 +1,65 @@
 <!DOCTYPE html>
 <html lang="de">
-  <!-- GENOCHECK bestellen -->
 
   <head>
     <meta charset="UTF-8">
     <title>GenoChoice&trade; - GenoCheck&trade; bestellen</title>
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
   </head>
 
   <body>
+    <div>
+      <ul class="navlist">
+        <li class="active"><a href="#">Phase 1</a></li>
+        <li><a href="#">Phase 2</a></li>
+        <li><a href="#">Phase 3</a></li>
+      </ul>
+    </div>
+
     <header>
-      <h1>GenoChoice</h1>
-      <hr>
+      <span class="headerTitle">Genochoice</span><br>
+      <span class="headerSubtitle">Legacy by Design</span>
     </header>
 
-    <h2>GenoCheck&trade; bestellen</h2>
-    <p>
-      Fordern Sie heute ihren <strong>kostenlosen</strong> GenoChoice&trade;-Gentest an.<br>
-      Ein Team aus professionellen Genforschern prüft mit unserem patentierten GenoCheck&trade;-Verfahren die Stärken und Schwächen ihres zukünftigen Kindes.
-    </p>
-    <section class="genoCheckOrder">
-      <form class="form" action="phase1tophase2.html" name="formGenoCheck[]" method="post">
-        <label for="inputFirstName">Vorname</label>
-        <input type="text" id="inputFirstName" name="inputFirstName" value="Max">
-        <label for="inputLastName">Name</label>
-        <input type="text" id="inputLastName" name="inputLastName" value="Mustermann"><br>
-        <label for="inputStreet">Straße und Hausnummer</label>
-        <input type="text" id="inputStreet" name="inputStreet" value="Musterstraße">
-        <input type="text" id="inputStreetNum" name="inputStreetNum" value="6" style="width:30px"><br>
-        <label for="inputCity">Stadt</label>
-        <input type="text" id="inputCity" name="inputCity" value="Musterstadt"><br>
-        <label for="inputZipcode">PLZ</label>
-        <input type="text" id="inputZipcode" name="inputZipcode" value="12345"><br>
-        <hr>
-        <button type="submit" onclick="alert('Bestellung aufgegeben!')">GenoCheck&trade; bestellen</button>
+    <section>
+      <span class="sectionHeader">GenoCheck&trade; bestellen</span>
+      <p>
+        Fordern Sie heute ihren <strong>kostenlosen</strong> GenoChoice&trade;-Gentest an.<br> Ein Team
+        aus professionellen Genforschern prüft mit unserem patentierten GenoCheck&trade;-Verfahren die
+        Stärken und Schwächen ihres zukünftigen Kindes.
+      </p>
+      <figure>
+        <img src="img/family.jpg" alt="Diese fröhliche Familie könnten Sie sein!">
+        <figcaption>Malte & Sombra Trontheim sind zufrieden mit ihrer GenoChoice&trade;-Entscheidung</figcaption>
+      </figure>
+    </section>
+    <section>
+      <span class="sectionHeader">Persönliche Daten</span>
+      <form id="genoCheckForm" name="genoCheckForm[]" action="phase1tophase2.html" method="post">
+        <label>Vorname</label>
+        <input type="text" id="inputFirstName" name="inputFirstName" value="Max" required autofocus>
+
+        <label>Name</label>
+        <input type="text" id="inputLastName" name="inputLastName" value="Musterhalfen" required>
+
+        <label>Straße & Hausnummer</label>
+        <input type="text" id="inputStreet" name="inputStreet" value="Musterstraße" required>
+
+        <label>Stadt</label>
+        <input type="text" id="inputCity" name="inputCity" value="Musterstadt" required>
+
+        <label>PLZ</label>
+        <input type="text" id="inputZipcode" name="inputZipcode" value="12345" pattern="\d{5}" required>
+
+        <label>E-Mail</label>
+        <input type="email" id="inputEmail" name="inputEmail" value="m.mustermann@gmail.com" required>
+
+        <button id="genoCheckSubmit" type="submit">
+          GenoCheck&trade; bestellen
+        </button>
       </form>
     </section>
   </body>
