bb7231004b5e53558d0ca651d38a09d27d3f3f5a
NoahK <nftrash@web.de>
2019-05-06 15:45:49 +0200

    Dynamische Statusabfrage bei Dropdown-Änderunng implementiert

--- phase1_agent.js
+++ phase1_agent.js
@@ -2,5 +2,37 @@
 document.getElementById("genoCheckOrdersSelect").addEventListener("change", loadStatus);
 
 function loadStatus() {
-  console.log("meeeemes");
+    let selectedUserid = document.getElementById("genoCheckOrdersSelect").selectedOptions[0].value;
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let statusJSON = JSON.parse(this.responseText);
+            if (statusJSON.length === 0) {
+                // Status nicht vorhanden
+                return;
+            }
+            // Status vorhanden
+            let selectedUserStatus = parseInt(statusJSON[0]);
+            setRadioGroupActive(selectedUserStatus);
+        }
+    };
+    xmlhttp.open("GET", "getstatus.php?userid="+selectedUserid, true);
+    xmlhttp.send();
+}
+
+/**
+ * "active"-Klasse von allen radioButtons entfernen
+ */
+function setRadioGroupActive(index) {
+    let radioButtons = document.getElementsByClassName("inputRadioGroup");
+
+    // alle "active"-Klassen entfernen
+    for (let i=0; i<radioButtons.length; i++) {
+        let classList = radioButtons[i].classList;
+        if (i === index && !classList.contains("active")) {
+            classList.add("active");
+        } else {
+            classList.remove("active");
+        }
+    }
 }
