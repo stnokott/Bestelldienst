a5b77c9f4f227a4f4857aa23a2174ce35030dc9a
NoahK <nftrash@web.de>
2019-05-06 11:51:27 +0200

    display: none fixed

---
+++ phase1_agent.js
@@ -0,0 +1,7 @@
+console.log("fuuf");
+document.getElementById("genoCheckOrdersSelect").addEventListener("change", loadStatus);
+
+function loadStatus() {
+  console.log("meeeemes");
+  document.getElementsByClassName("agentName")[0].innerHTML = "fuuf";
+}
