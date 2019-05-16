ab991d3cc4af816827509d4ffbb1efd1437c4e84
golleboy <golleboy@t-online.de>
2019-05-16 17:34:22 +0200

    phase4.php erstellt

---
+++ phase4.php
@@ -0,0 +1,18 @@
+<?php	// UTF-8 marker äöüÄÖÜß€
+/**
+ * @category File
+ * @package  Bestelldienst
+ * @author   Noah Kottenhahn, <noah.kottenhahn@stud.h-da.de>
+ * @author   Max Klosterhalfen, <max.klosterhalfen@stud.h-da.de>
+ * @license  http://www.h-da.de  none
+ */
+
+/*
+vgl post query usererstellung
+Post Argumente Validieren
+Genochoiceordereintrag erstellen mit userid + post Info
+  Post-Info:
+    kittype=0/1/2/3/4
+    optionals=[0/1/2, 0/1/2, ...]
+Für jede optional eintrag in die orderoptionalsdatenbank
+*/
